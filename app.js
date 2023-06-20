c4ca47d3bcee34b650797de34ad12c5c434fa592
gaeddogida <dd5giee@gmail.com>
2023-06-20 11:43:54 +0900

    Please enter the commit message for your changes. Lines starting

--- app.js
+++ app.js
@@ -1,39 +1,45 @@
-'use strict'
+'use strict';
 
 const express = require('express'),
     path = require('path'),
     bodyParser = require('body-parser'),
     session = require('express-session'),
-//    logger = require('@amuzlab/logger'),
+    //    logger = require('@amuzlab/logger'),
 
-    routes = require('./routes')
+    routes = require('./routes');
 
-module.exports = exports = express()
+module.exports = exports = express();
 
 exports
     .use(require('cors')())
-//    .use(express.static(path.join(__dirname, 'public')))
-    .use(bodyParser.json({
-        limit: '10mb'
-    }))
-    .use(bodyParser.urlencoded({
-        limit: '10mb',
-        extended: false
-    }))
+    //    .use(express.static(path.join(__dirname, 'public')))
+    .use(
+        bodyParser.json({
+            limit: '10mb',
+        })
+    )
+    .use(
+        bodyParser.urlencoded({
+            limit: '10mb',
+            extended: false,
+        })
+    )
     .use(require('cookie-parser')())
-    .use(session({
-        secret: 'keyboard cat',
-        resave: true,
-        saveUninitialized: false,
-        cookie: {
-            maxAge: null,
-            httpOnly: true
-        },
-        store: new (require('session-memory-store')(session))()
-    }))
+    .use(
+        session({
+            secret: 'keyboard cat',
+            resave: true,
+            saveUninitialized: false,
+            cookie: {
+                maxAge: null,
+                httpOnly: true,
+            },
+            store: new (require('session-memory-store')(session))(),
+        })
+    )
     .use(
         '/',
-        routes//,
+        routes //,
         // express.static(
         //     path.join(__dirname, 'public', 'views'),
         //     {
@@ -41,11 +47,13 @@ exports
         //     })
     )
     .use((err, req, res, next) => {
-        logger.error('server error (err : %s)', err.stack)
+        logger.error('server error (err : %s)', err.stack);
 
         if (res.statusCode !== 404) {
-            res
-                .status(err.statusCode ? err.statusCode : 500)
-                .send(err instanceof Error ? err.message : err)
+            res.status(err.statusCode ? err.statusCode : 500).send(
+                err instanceof Error ? err.message : err
+            );
         }
-    })
+    });
+
+// test!
